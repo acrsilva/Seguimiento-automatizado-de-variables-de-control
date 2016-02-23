f5a871783d7e1ddf85e79cc2c5a84e32b6658bcd
alallegue <alvaroallegue@gmail.com>
2016-02-23 21:34:30 +0100

    dinamica

--- pruebas/pruebainterfazdinamicabar.py
+++ pruebas/pruebainterfazdinamicabar.py
@@ -9,3 +9,40 @@ compilar previamente
 """
 
 
+#import initExample ## Add path to library (just for examples; you do not need this)
+
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore, QtGui
+import numpy as np
+import os
+
+pg.mkQApp()
+
+## Define main window class from template
+path = os.path.dirname(os.path.abspath(__file__))
+uiFile = os.path.join(path, 'intbarras.ui')
+WindowTemplate, TemplateBaseClass = pg.Qt.loadUiType(uiFile)
+
+class MainWindow(TemplateBaseClass):  
+    def __init__(self):
+        TemplateBaseClass.__init__(self)
+        self.setWindowTitle('pyqtgraph example: Qt Designer')
+        
+        # Create the main window
+        self.ui = WindowTemplate()
+        self.ui.setupUi(self)
+        #self.ui.plotBtn.clicked.connect(self.plot)
+        
+        self.show()
+        
+    def plot(self):
+        self.ui.plot.plot(np.random.normal(size=100), clear=True)
+        
+win = MainWindow()
+
+
+## Start Qt event loop unless running in interactive mode or using pyside.
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
