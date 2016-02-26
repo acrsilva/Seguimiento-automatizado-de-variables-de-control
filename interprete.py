ff82c1b2123bd28d99b79aba158171e128106bcc
alallegue <alvaroallegue@gmail.com>
2016-02-26 22:16:45 +0100

    grafica actividad fisica

--- interprete.py
+++ interprete.py
@@ -20,18 +20,36 @@ path = os.path.dirname(os.path.abspath(__file__))
 uiFile = os.path.join(path, 'interfaz.ui')
 WindowTemplate, TemplateBaseClass = pg.Qt.loadUiType(uiFile)
 
+
 class MainWindow(TemplateBaseClass):  
     def __init__(self):
         TemplateBaseClass.__init__(self)
         self.ui = WindowTemplate()
         self.ui.setupUi(self)
-        #self.ui.plotBtn.clicked.connect(self.plot)
         
         #Configurar la gráfica de barras de episodios de sueño
-        plt = pg.PlotItem()
-        plt.getViewBox().setMouseEnabled(False, False)
-        self.ui.plotBarSueno.setCentralItem(plt)
-        plt.addItem(barGraphItem)
+        self.plt = pg.PlotItem()
+        self.plt.getViewBox().setMouseEnabled(False, False)
+        self.ui.plotBarSueno.setCentralItem(self.plt)
+        self.selep = selecepisodio.SelecEpisodio()
+        self.barSuenio = self.selep.barSuenio
+        self.plt.addItem(self.barSuenio)
+        
+        #Configurar las gráficas de consumo energético
+        win = self.ui.plotConsumo
+        self.barConsumo = self.selep.barConsumo
+        self.p1 = win.addPlot()
+        self.p1.addItem(self.barConsumo)
+        
+        win.nextRow()
+        
+        #p2 = win.addPlot()
+        #p2.addItem(self.barSuenio)
+        
+        #Configurar los botones
+        self.ui.next_e_btn.clicked.connect(self.nextEp)
+        self.ui.prev_e_btn.clicked.connect(self.prevEp)
+        
         """
         #Configurar la gráfica de consumo energético
         self.ui.plotConsumo.setLabel('left', 'Tipo de sueño', units='');
@@ -39,16 +57,24 @@ class MainWindow(TemplateBaseClass):
         self.ui.plotConsumo.setLabel('left', 'Tipo de sueño', units='');
         self.ui.plotConsumo.setLabel('bottom', 'Instante', units='minutos');
         """
+        
         self.show()
         
-    def plot(self):
-        self.ui.plot.plot(np.random.normal(size=100), clear=True)
-        
+    def nextEp(self):
+        self.plt.clear()
+        self.selep.episodioSiguiente()
+        self.barSuenio = self.selep.barSuenio
+        self.plt.addItem(self.barSuenio)
+        self.p1.clear()
+        self.p1.addItem(self.selep.barConsumo)
+    def prevEp(self):
+        self.plt.clear()
+        self.selep.episodioAnterior()
+        self.barSuenio = self.selep.barSuenio
+        self.plt.addItem(self.barSuenio)
+        self.p1.clear()
+        self.p1.addItem(self.selep.barConsumo)
 
-selep = selecepisodio.SelecEpisodio()
-barGraphItem = selep.barGraphItem
-
-x = selep.getX()
 
 #Inicializar interfaz
 win = MainWindow()
--- selecepisodio.py
+++ selecepisodio.py
@@ -14,35 +14,35 @@ import numpy as np
 
 #Cargar los datos
 csv = np.genfromtxt ('data.csv', delimiter=",")
-x = csv[:,1]
-y = csv[:,25]
+x = csv[:,1] #Tiempo
+y = csv[:,25] #Clasificador de sueño
+z = csv[:,24] #Clasificador de actividad 1, 2, 3, 4, 5, 7, 9
 
 def trocear(y):
-	print "troceando, num y: %i" % len(y)
-	indices = []
-	a = False
-	c = 0
-	f = 0
-	for i in range(len(y)):
-		if(not a and y[i] != 0): #nuevo episodio
-			c = i 
-			a = True
-		elif(a): #dormido->despierto
-			if(y[i] !=0): #dormido(resetear tiempo despierto)
-				f = 0
-			else: #despierto(cuanto tiempo?)
-				if(f < 60):
-					f = f + 1
-				else: #fin del episodio (1h seguida despierto)
-					f = i
-					indices.append([c, f])
-					a = False
-					c = 0
-					f = 0
-	return indices
+    print "troceando, num y: %i" % len(y)
+    indices = []
+    a = False
+    c = 0
+    f = 0
+    for i in range(len(y)):
+        if(not a and y[i] != 0): #nuevo episodio
+            c = i 
+            a = True
+        elif(a): #dormido->despierto
+            if(y[i] !=0): #dormido(resetear tiempo despierto)
+                f = 0
+            elif(f < 60): #despierto(cuanto tiempo?)
+                f = f + 1
+            else: #fin del episodio (1h seguida despierto)
+                f = i
+                indices.append([c, f])
+                a = False
+                c = 0
+                f = 0
+    return indices
 
-def colorea():
-	print "coloreando"
+def coloreaSueno():
+	print "coloreando barras de sueño"
 	colors = []
 	num = 0
 	for i in y:
@@ -52,13 +52,36 @@ def colorea():
 			c = pg.mkColor(255, 255, 0)
 		elif(i == 5): #Sueño muy profundo - Verde
 			c = pg.mkColor(0, 255, 0)
-			#print "Verde! en %i\n" % num
 		else: #Despierto - Rojo
 			c = pg.mkColor(255, 0, 0)
 		colors.append(c)
 		num = num + 1
 	print "num colores %i" % len(colors)	
-	return colors	
+	return colors
+    
+def coloreaConsumo():
+    print "coloreando barras de actividad fisica"
+    colors = []
+    num = 0
+    for i in z:
+        if(i == 1):
+            c = pg.mkColor(255, 255, 255)
+        elif(i == 2):
+            c = pg.mkColor(255, 255, 0)
+        elif(i == 3):
+            c = pg.mkColor(0, 255, 0)
+        elif(i == 4):
+            c = pg.mkColor(128, 255, 0)
+        elif(i == 5):
+            c = pg.mkColor(0, 255, 128)
+        elif(i == 7):
+            c = pg.mkColor(0, 255, 255)    
+        else:
+            c = pg.mkColor(255, 0, 255)
+        colors.append(c)
+        num = num + 1
+    print "num colores %i" % len(colors)	
+    return colors
 
 def imprimeIndicesEp(indices):
 	print "Imprimiendo indices..."
@@ -67,43 +90,59 @@ def imprimeIndicesEp(indices):
 		print "indice %i : %i - %i" % (k, i[0], i[1]) 
 		k = k+1
 	
-
-class SelecEpisodio(object):
-	#Obtener indices de cada episodio de todo el intervalo de sueño
-	indices = trocear(y)
-	
-	imprimeIndicesEp(indices)
-	
-	colors = colorea()
-		
-	#Elegir episodio a mostrar
-	a = indices[0][0]
-	b = indices[0][1]
+def creaSuenoBar(ep, indices, colors):
+	a = indices[ep][0]
+	b = indices[ep][1]
 	n = b-a
 	xp = x[a:b]
 	yp = y[a:b]
 	xGrid = np.linspace(xp[0], xp[-1], n)
 	yGrid = np.interp(xGrid, xp, yp)	
-	
-		
 	barGraphItem = pg.BarGraphItem()
 	barGraphItem.setOpts(x0=xGrid[:-1], x1=xGrid[1:], y0=[0] * n, height=1, brushes=colors[a:b], pens=colors[a:b])
+	return barGraphItem
 	
-	consumoBar = pg.BarGraphItem()
-	consumoBar.setOpts(x0=xGrid[:-1], x1=xGrid[1:], y0=[0] * n, height=1, brushes=colors, pens=colors)
+def creaConsumoBar(ep, indices, colors):
+    a = indices[ep][0] - (3 * 60)
+    b = indices[ep][1]
+    n = b-a
+    xp = x[a:b]
+    zp = z[a:b]
+    xGrid = np.linspace(xp[0], xp[-1], n)
+    zGrid = np.interp(xGrid, xp, zp)	
+    barGraphItem = pg.BarGraphItem()
+    barGraphItem.setOpts(x0=xGrid[:-1], x1=xGrid[1:], y0=[0] * n, height=1, brushes=colors[a:b], pens=colors[a:b])
+    return barGraphItem
 
-	def episodioSiguiente(cls):
-		print "hola"
-		
-	#def episodioAnterior(cls):	
-		
-	def getSuenioBar(cls):
-		return cls.barGraphItem
-	
-	def getConsumoBar(cls):
-		return cls.barConsumo
-		
-	def getX(cls):
-		return x	
+def creaConsumoGraf(ep, indices):
+    print "creando grafica de consumo energético"
+
+class SelecEpisodio(object):
+    #Obtener indices de cada episodio de todo el intervalo de sueño
+    indices = trocear(y)
+    imprimeIndicesEp(indices)
+    colors = coloreaSueno()
+    consColors = coloreaConsumo()
+
+    #Elegir el episodio inicial
+    epAct = 0
+    barSuenio = creaSuenoBar(epAct, indices, colors)
+    barConsumo = creaConsumoBar(epAct, indices, consColors)
+    grafConsumo = creaConsumoGraf(epAct, indices)
+
+    def episodioSiguiente(cls):
+        if (cls.epAct < len(cls.indices) - 1): #Último episodio
+            cls.epAct += 1
+            cls.barSuenio = creaSuenoBar(cls.epAct, cls.indices, cls.colors)
+            cls.barConsumo = creaConsumoBar(cls.epAct, cls.indices, cls.consColors)
+        print "siguiente episodio: %i" % cls.epAct
+
+    def episodioAnterior(cls):
+        if (cls.epAct > 0): #Primer episodio
+            cls.epAct -= 1
+            cls.barSuenio = creaSuenoBar(cls.epAct, cls.indices, cls.colors)
+            cls.barConsumo = creaConsumoBar(cls.epAct, cls.indices, cls.consColors)
+        print "anterior episodio: %i" % cls.epAct
+
 
 		
