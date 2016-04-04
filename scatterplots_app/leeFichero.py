b52541b1ca31db706029e0a3aa49ee716df2fef2
acrsilva <acrsilva@ucm.es>
2016-04-04 23:45:23 +0200

    Lee fichero ahora funciona

--- scatterplots_app/leeFichero.py
+++ scatterplots_app/leeFichero.py
@@ -5,24 +5,27 @@ import numpy as np
 import codecs
 
 class LeeFichero(object):
+    """
+    Inicializa la matriz con los valores del csv
     
+    Parametros de entrada
+    - nombre: nombre del fichero csv que contiene los datos
+    """
     def __init__(self, nombre):
         self.nombreFichero = nombre
         self.csv = np.genfromtxt(nombre, delimiter="," , names=True)
         self.nomCols = self.csv.dtype.names
         self.nparams = len(self.nomCols)
-        self.sueno = self.csv[self.nomCols[15]]#['Sueño']
-        self.flujo = self.csv[self.nomCols[self.nparams-2]] #['Flujo_térmico__media]
+        self.sueno = self.csv['Sueño'.encode('utf-8')]
+        self.flujo = self.csv['Flujo_térmico__media'.encode('utf-8')]
         self.temp = self.csv['Temp_cerca_del_cuerpo__media']
         self.tiempo = self.csv['Time']
         self.actli = self.csv['Ligera']
         self.actsd = self.csv['Sedentaria']
         self.actmd = self.csv['Moderada']
-        self.consm = self.csv[self.nomCols[17]] #['Gasto_energético']
+        self.consm = self.csv['Gasto_energético'.encode('utf-8')]
         self.acltrans = self.csv['Acel_transversal__picos']
-        
-
-
+"""
 lee = LeeFichero('../data.csv')
 print lee.nombreFichero
 print lee.nparams
@@ -37,3 +40,4 @@ print lee.nomCols[15]
 print lee.sueno
 print lee.nomCols[17]
 print lee.consm
+"""
