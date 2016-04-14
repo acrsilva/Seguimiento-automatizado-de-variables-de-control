96a2ef78b65f7b6ab82df20c4ee0a29929e1bdf3
alallegue <alvaroallegue@gmail.com>
2016-04-14 11:41:25 +0200

    clustering

--- estudio_flujos_app/main.py
+++ estudio_flujos_app/main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import cachitos
 from matplotlib import pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
@@ -9,6 +10,7 @@ from scipy.spatial.distance import pdist
 from sklearn import preprocessing
 from scipy.spatial.distance import euclidean
 from fastdtw import fastdtw
+import scipy.spatial.distance as ssd
 
 
 class Individuo:
@@ -41,22 +43,25 @@ for i in csv_sueno:
 
 
 #Calcular distancias
-distancias = [] #Matriz de distancias entre individuos
+s = len(eps_sueno)
+print s
+distancias = np.zeros((s, s)) #Matriz de distancias entre individuos
 x, y = 0, 0
 for i in eps_sueno:
     for j in eps_sueno:
-        distanceTemp = fastdtw(i.stt, j.stt, dist=euclidean) #Distancia en temperatura
-        distanceFlujo = fastdtw(i.stf, j.stf, dist=euclidean) #Distancia en flujo
-        distancias.append(distanceTemp + distanceFlujo) #Distancia euclídea total
+        distanceTemp , path = fastdtw(i.stt, j.stt, dist=euclidean) #Distancia en temperatura
+        distanceFlujo , path = fastdtw(i.stf, j.stf, dist=euclidean) #Distancia en flujo
+        distancias[y][x] = distanceTemp + distanceFlujo #Distancia euclídea total
+        distancias[x][y] = distancias[y][x]
         x += 1
+    x = 0
     y += 1
 
 
-#plt.scatter(X[:,0], X[:,1])
-#plt.show()
 
 #Vector con las distancias requeridas para hacer clustering
 print len(distancias), "distancias"
+print distancias.shape
 
 """
 Resultados:
@@ -68,10 +73,12 @@ weighted: 0.816403408353
 median: 0.772500661416
 ward: 0.823703775985
 """
-Z = linkage(distancias, 'centroid')
+dists = ssd.squareform(distancias)
+Z = linkage(dists, 'centroid')
 
 
-c, coph_dists = cophenet(Z, pdist(X))
+#c, coph_dists = cophenet(Z, pdist(X))
+c, coph_dists = cophenet(Z, distancias)
 
 print c
 
