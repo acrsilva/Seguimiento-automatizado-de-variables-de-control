8885e76308fabffdc8965a8929203f9136d7a38e
alallegue <alvaroallegue@gmail.com>
2016-05-09 19:44:08 +0200

    barra diario

--- estudio_consumo_app/estudio_consumo.py
+++ estudio_consumo_app/estudio_consumo.py
@@ -26,6 +26,7 @@ class Main(QMainWindow, Ui_MainWindow):
         self.__initCombobox__()
         self.initGraphs()
         self.plotGraph(izq=True)
+        self.plotBarDiario()
         
         #self.drawActividadesPie()
         #self.drawConsumosPie()
@@ -35,8 +36,8 @@ class Main(QMainWindow, Ui_MainWindow):
     
     def __initCombobox__(self):
         for i in range(len(self.selep.epsDias)):
-            self.cbx_izq.addItem("Día " + str(i))
-            self.cbx_der.addItem("Día " + str(i))
+            self.cbx_izq.addItem("Día " + str(i+1))
+            self.cbx_der.addItem("Día " + str(i+1))
     
     def initGraphs(self):
         print "Inicializar gráficas"
@@ -48,8 +49,8 @@ class Main(QMainWindow, Ui_MainWindow):
         
         #Gráficas día izquierdo
         self.fig_izq = plt.figure(tight_layout=True)
-        canvas_izq = FigureCanvas(self.fig_izq)
-        self.layout_dia_izq.addWidget(canvas_izq)
+        self.canvas_izq = FigureCanvas(self.fig_izq)
+        self.layout_dia_izq.addWidget(self.canvas_izq)
         
         #Gráficas día derecho
         self.fig_der = plt.figure(tight_layout=True)
@@ -60,24 +61,96 @@ class Main(QMainWindow, Ui_MainWindow):
         if(izq):
             labels = ['Dormido', 'Sedentario', 'Act. Ligera', 'Act. Moderada']
             colors = [colores.sueno, colores.sedentario, colores.ligero, colores.moderado]
-            print self.cbx_izq.currentIndex()
+            #Gráfica de tiempos
             sizes = self.getSizes(self.cbx_izq.currentIndex(), tiempo=True)
             ax_tiempo = self.fig_izq.add_subplot(221)
             pie = ax_tiempo.pie(sizes, colors=colors, autopct='%1.1f%%', shadow=False, startangle=0)
-            #ax_tiempo.legend(pie[0], labels, loc="upper left")
+            ax_tiempo.legend(pie[0], labels, loc="upper left", prop={'size':7})
             ax_tiempo.axis('equal')
-        
+            
+            #Gráfica de consumos
             sizes = self.getSizes(self.cbx_izq.currentIndex(), consumo=True)
             ax_consumo = self.fig_izq.add_subplot(222)
             pie = ax_consumo.pie(sizes, colors=colors, autopct='%1.1f%%', shadow=False, startangle=0)
             #ax_consumo.legend(pie[0], labels, loc="upper left")
             ax_consumo.axis('equal')
             
+            #Gráfica de ratios
             ax_bar= self.fig_izq.add_subplot(212)
             
-            
+            ratios = []
+            idx = self.cbx_izq.currentIndex()
+            for i in self.selep.epsDias[idx]:
+                ratios.append(i.numCalorias / (i.fin - i.ini))
         
+            self.plotRatioBar(ax_bar, idx, ratios)
     
+    def plotBarDiario(self):
+        """
+        suenos = [0]*len(self.selep.epsDias)
+        sedentarias = [0]*len(self.selep.epsDias)
+        ligeras = [0]*len(self.selep.epsDias)
+        moderadas = [0]*len(self.selep.epsDias)
+        """
+        suenos = np.empty(len(self.selep.epsDias))
+        sedentarias = np.empty(len(self.selep.epsDias))
+        ligeras = np.empty(len(self.selep.epsDias))
+        moderadas = np.empty(len(self.selep.epsDias))
+        idx = 0
+        for j in self.selep.epsDias:
+            for i in j:
+                if(i.tipo == cachitos.tipoSueno):
+                    suenos[idx] += i.numCalorias
+                elif(i.tipo == cachitos.tipoSedentario):
+                    sedentarias[idx] += i.numCalorias
+                elif(i.tipo == cachitos.tipoLigera):
+                    ligeras[idx] += i.numCalorias
+                elif(i.tipo == cachitos.tipoModerado):
+                    moderadas[idx] += i.numCalorias
+            idx += 1
+        print suenos    
+        print suenos + sedentarias
+        ind = np.arange(len(self.selep.epsDias))
+        self.fig_barDiario.axes[0].bar(ind, suenos, color=colores.sueno)
+        self.fig_barDiario.axes[0].bar(ind, sedentarias, bottom=suenos, color=colores.sedentario)
+        self.fig_barDiario.axes[0].bar(ind, ligeras, bottom=suenos+sedentarias, color=colores.ligero)
+        self.fig_barDiario.axes[0].bar(ind, moderadas, bottom=suenos+sedentarias+ligeras, color=colores.moderado)
+        
+    def plotRatioBar(self, ax, idx, means):
+        def onpick(event):
+            rect = event.artist
+            for i in range(len(bar)): #MEJORAR!!
+                if (bar[i] == rect):
+                    print "Barra", i, self.selep.epsDias[idx][i].nombre, self.selep.epsDias[idx][i].numCalorias, 'calorías', len(self.selep.epsDias[idx][i].tiempo), 'minutos'
+                    #lbl = '(' + self.selep.epFiltro[i].nombre + ') ' + str(self.selep.epFiltro[i].numCalorias)[:6] + ' calorías ' + str(len(self.selep.epFiltro[i].tiempo)) + ' minutos'
+                    #self.lblDetalles.setText(lbl)
+                    return
+                    
+        colors = []
+        for i in self.selep.epsDias[idx]:
+            if(i.tipo == cachitos.tipoSueno):
+                c = colores.sueno
+            elif(i.tipo == cachitos.tipoSedentario):
+                c = colores.sedentario
+            elif(i.tipo == cachitos.tipoLigera):
+                c = colores.ligero
+            elif(i.tipo == cachitos.tipoModerado):
+                c = colores.moderado
+            colors.append(c)
+        
+        print len(means), "muestras"    
+        ind = np.linspace(0, len(means), endpoint=False, num=len(means))
+        bar = ax.bar(ind, means, color=colors, picker=1, align='center')
+        ax.set_xticklabels(np.arange(len(means)))
+        #fig.tight_layout()
+        
+        #canvas = FigureCanvas(fig)
+        #vbox = QtGui.QGridLayout()
+        #vbox.addWidget(canvas)
+        #canvas.mpl_connect('pick_event', onpick)
+        self.canvas_izq.mpl_connect('pick_event', onpick)
+        
+            
     def __crearPieWidget__(self, sizes):
         #labels = ['Dormido', 'Sedentario', 'Act. Ligera', 'Act. Moderada', 'Act. Intensa', 'Act. Muy intensa']
         labels = ['Dormido', 'Sedentario', 'Act. Ligera', 'Act. Moderada']
